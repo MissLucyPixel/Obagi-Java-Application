d47c4383da061f0cc7aa31cc3610d04507f1a9e1
MissLucyPixel <ibklucy@gmail.com>
2025-03-27 19:38:01 +0100

    For Loop

---
+++ ForloopStatement.java
@@ -0,0 +1,9 @@
+public class ForloopStatement{
+	public static void main(String[] args){
+		int num = 50;
+		
+		for(int i = 1; i <= 10; i++){
+			System.out.printf("%d%n",num);
+		}
+	}
+}
---
+++ PrintName.java
@@ -0,0 +1,9 @@
+public class PrintName{
+	public static void main(String[] args){
+		String name  = "Femi";
+		
+		for(int i = 1; i <= 10; i++){
+			System.out.printf("%s %n",name);
+		}
+	}
+}
